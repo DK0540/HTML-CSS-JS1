edb486a8de66e19612e24cc4278e880b606cce57
Tutorial-git <dk0540@digikull.com>
2022-10-27 01:57:46 +0530

    updated

---
+++ js/day1/script.js
@@ -0,0 +1,59 @@
+// console.log("Hello world");
+
+// var a = 5;
+// var a = 10;
+
+// let b = 10;
+// const c = 5 - a;
+
+// var z = a + b;
+
+// var k;
+// k = 50;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// console.log(z);
+
+// console.log(typeof z);
+
+// let string = "hello";
+// let show = "Name";
+
+// console.log(string);
+// console.log(show);
+
+var a = 10;
+var a = 12;
+console.log(a);
+
+let g = 100;
+let h = 200;
+console.log(g);
+console.log(h);
+
+const greetToday = "hello good mlorning";
+console.log(greetToday);
+
+// var/let/cont = variable name;
+// a/j/greetName == Identifiers;
+// 100/"hello"/   == variable value;
+
+let $greet = "Good afternoon";
+console.log($greet);
+
+var new_variable = 5;
+var c = 100 + new_variable;
+let data = "datavalue";
+
+console.log(data);
+
+console.log(num);
+var num = "Hello";
+console.log(num);
+
+//dataTypes
+//Scope
+//
